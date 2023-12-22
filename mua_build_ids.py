a65480957acd7b23bcafd2826e03f97dcd81edd2
phi-go <github@philipp-goerz.com>
2023-12-22 15:27:25 +0100

    update mua_build_ids to be preemptible

--- mua_build_ids.py
+++ mua_build_ids.py
@@ -2,70 +2,97 @@
 
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from dataclasses import dataclass
 import fcntl
 import os, argparse, random, subprocess, json
+import sqlite3
 from pathlib import Path
 from multiprocessing import Pool
 import tempfile
+import time
 import traceback
+import uuid
+
 
 MAPPED_DIR = Path('/tmp/experiment-data/')
-
-# POOL_SIZE = 10
-
-
-# for each mutant compilation
-# 1. check if mutant is already compiled, if yes, exit
-# 2. create lock file in tmp dir
-# 3. check if mutant is already compiled, if yes, exit
-# 4. if mutant (.tmp) file exists, delete
-# 5. (re)create config file
-# 6. build mutant to (.tmp)
-# 7. rename mutant remove (.tmp)
-# 8. delete config file
-# 9. delete lock file
+LOCATOR_TIMEOUT = 30
 
 
-def acquireLock(path):
-    ''' acquire exclusive lock file access '''
-    locked_file_descriptor = open(path, 'w+')
-    try:
-        fcntl.lockf(locked_file_descriptor, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except OSError:
-        # another instance already locked this
-        return None
-    return locked_file_descriptor
+@dataclass
+class CompilationStatus:
+    exec_id: int
+    done: bool
 
-def releaseLock(locked_file_descriptor):
-    ''' release exclusive lock file access '''
-    locked_file_descriptor.close()
 
-def build_mutant(mut_id, mutants_dir, fuzz_target, debug_num_mutants):
+class CompileDB:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.conn = sqlite3.connect(db_file, check_same_thread=False)
+
+    def initialize(self):
+        print(f"Initializing compile db {self.db_file}")
+        cur = self.conn.cursor()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS compilations (
+                mut_id INTEGER PRIMARY KEY,
+                exec_id BLOB,
+                done INTEGER
+            )
+        ''')
+        cur.execute('PRAGMA journal_mode=WAL')
+        cur.execute('PRAGMA synchronous=NORMAL')
+        self.conn.commit()
+
+    def get_compilation_status(self, mut_id):
+        cur = self.conn.cursor()
+        cur.execute('SELECT exec_id, done FROM compilations WHERE mut_id=?', (mut_id,))
+        res = cur.fetchall()
+        all = []
+        for r in res:
+            all.append(CompilationStatus(r[0], r[1]))
+        return all
+
+    def get_num_compiled(self, exec_id):
+        cur = self.conn.cursor()
+        cur.execute('SELECT COUNT(*) FROM compilations WHERE done=1 or exec_id=?', (exec_id,))
+        return cur.fetchone()[0]
+
+    def set_started_building(self, mut_id, exec_id):
+        cur = self.conn.cursor()
+        cur.execute('INSERT INTO compilations (mut_id, exec_id, done) VALUES (?, ?, 0)', (mut_id, exec_id))
+        self.conn.commit()
+
+    def set_finished_building(self, mut_id):
+        cur = self.conn.cursor()
+        cur.execute('UPDATE compilations SET done=1 WHERE mut_id=?', (mut_id,))
+        self.conn.commit()
+
+
+def build_mutant(mut_id, exec_id, compile_db_path, mutants_dir, fuzz_target, debug_num_mutants):
+    compile_db = CompileDB(compile_db_path)
 
     if debug_num_mutants is not None:
-        num_lock_files = len(list(mutants_dir.glob("*.lock")))
-        num_mutant_binaries = len(list(filter(
-            lambda ff: ff.suffix != '.lock' and ff.suffix != '.json' and ff.is_file(),
-            mutants_dir.glob("*"))
-        ))
-        if num_lock_files + num_mutant_binaries >= debug_num_mutants:
+        num_compiled = compile_db.get_num_compiled(exec_id)
+        if num_compiled >= debug_num_mutants:
             return
 
-    # built covered mutant if not build yet
-    mutant_lock = mutants_dir/f'{mut_id}.lock'
-    lock_fd = acquireLock(mutant_lock)
-    if lock_fd is None:
-        # lock is already taken
-        return
-
-    # if mutant exists, do not build
     mutant_file = mutants_dir/f'{mut_id}'
+
+    # check if mutant is already compiled
+    compilation_status = compile_db.get_compilation_status(mut_id)
+    for cs in compilation_status:
+        if cs.done is True:
+            print("mutant is already compiled")
+            if not mutant_file.is_file():
+                print(f"Error: mutant_file: {mutant_file} does not exist")
+            return
+        if cs.exec_id == exec_id:
+            return
+
+    # mark mutant as being compiled
+    compile_db.set_started_building(mut_id, exec_id)
+
     config_file = mutant_file.with_suffix(".json")
-    
-    if(os.path.exists(mutant_file)):
-        return
-    if(os.path.isfile(config_file)):
-        return
     
     # create config file
     with open(config_file, "w") as f:
@@ -94,15 +121,26 @@ def build_mutant(mut_id, mutants_dir, fuzz_target, debug_num_mutants):
         env = os.environ.copy()
         env['MUT_SHARED_DIR'] = tmp_dir
         env['MUT_HOST_TMP_PATH'] = tmp_dir
-        subprocess.run(
-            build_command,
-            cwd='/mutator',
-            env=env,
-        )
+        try:
+            subprocess.run(
+                build_command,
+                cwd='/mutator',
+                env=env,
+                stderr=subprocess.STDOUT,
+                stdout=subprocess.PIPE,
+                errors='replace',
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error while building mutant: {e.output}")
+            return
+        finally:
+            #cleanup
+            os.remove(config_file)
 
-    #cleanup
-    os.remove(config_file)
-    releaseLock(lock_fd)
+    # mark mutant as compiled
+    compile_db.set_finished_building(mut_id)
+
 
 def locate_corpus_entry(corpus_entry, locator_path, corpus_dir, mutants_ids_dir):
     input_file = os.path.join(corpus_dir, corpus_entry)
@@ -121,7 +159,11 @@ def locate_corpus_entry(corpus_entry, locator_path, corpus_dir, mutants_ids_dir)
             subprocess.run(
                 [str(locator_path), str(input_file)],
                 cwd=trigger_dir,
-                timeout=10,
+                timeout=LOCATOR_TIMEOUT,
+                stderr=subprocess.STDOUT,
+                stdout=subprocess.PIPE,
+                errors='replace',
+                check=True,
             )
         except subprocess.CalledProcessError as e:
             errored_out = str(e)
@@ -154,6 +196,9 @@ def main():
                     description='Script runs generated corpus against locator targets to get a list of covered mutants. Then, covered mutants are build, if not already existent',
                     epilog='')
 
+    parser.add_argument('exec_id', metavar='I',
+                    help='fuzzbench fuzz_target')
+
     parser.add_argument('fuzz_target', metavar='T',
                     help='fuzzbench fuzz_target')
 
@@ -171,6 +216,7 @@ def main():
 
     args = parser.parse_args()
 
+    exec_id = uuid.UUID(args.exec_id)
     fuzz_target = args.fuzz_target
     experiment = args.experiment
     fuzzer = args.fuzzer
@@ -181,12 +227,18 @@ def main():
     corpus_dir = shared_mua_binaries_dir / 'corpi' / fuzzer / trial_num
     mutants_ids_dir = shared_mua_binaries_dir / 'mutant_ids' / fuzzer / trial_num
 
+    compile_db_path = shared_mua_binaries_dir / 'compile.sqlite'
     mutants_dir = shared_mua_binaries_dir / 'mutants'
     locator_path = (Path('/out') / fuzz_target).with_suffix('.locator')
 
     print(f"mutants_ids_dir_entry: {mutants_ids_dir}")
 
+    compile_db = CompileDB(compile_db_path)
+    compile_db.initialize()
+
     all_mut_ids = set()
+
+    locate_start_time = time.time()
 
     # execute corpus with locator
     corpus_list = os.listdir(corpus_dir)
@@ -219,15 +271,24 @@ def main():
                 successful_count += 1
         completed_count += 1
         if completed_count % 100 == 0:
-            print(f"Completed {completed_count}/{corpus_len} corpus entries, {successful_count} successful, {timeout_count} timed out, {errored_count} errored out")
+            print(f"Completed {completed_count}/{corpus_len} corpus entries in {time.time() - locate_start_time:.2f}s, {successful_count} successful, {timeout_count} timed out, {errored_count} errored out", flush=True)
 
-    print(f"Locator: All completed for {fuzz_target} {experiment} {fuzzer} {trial_num}: {completed_count}/{corpus_len} corpus entries. New results: {successful_count} successful, {timeout_count} timed out, {errored_count} errored out.")
+    print(f"Locator: All completed for {fuzz_target} {experiment} {fuzzer} {trial_num}: {completed_count}/{corpus_len} corpus entries in {time.time() - locate_start_time:.2f}s. New results: {successful_count} successful, {timeout_count} timed out, {errored_count} errored out.")
+
+    exec_id_bytes = exec_id.bytes
+
+    if debug_num_mutants is not None and len(all_mut_ids) > debug_num_mutants:
+        print(f"for debugging reducing num of all_mut_ids: {len(all_mut_ids)} to {debug_num_mutants}")
+        all_mut_ids = random.sample(list(all_mut_ids), debug_num_mutants)
+        print(f"all_mut_ids: {len(all_mut_ids)}")
+
+    build_start_time = time.time()
 
     # build mutants
     build_jobs = []
     with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
         for mut_id in all_mut_ids:
-            build_jobs.append(executor.submit(build_mutant, mut_id, mutants_dir, fuzz_target, debug_num_mutants))
+            build_jobs.append(executor.submit(build_mutant, mut_id, exec_id_bytes, compile_db_path, mutants_dir, fuzz_target, debug_num_mutants))
 
     for job in as_completed(build_jobs):
         try:
@@ -235,6 +296,8 @@ def main():
         except Exception as e:
             stacktrace = traceback.format_exc()
             print(f"Error while building mutant: {e}:\n{stacktrace}")
+
+    print(f"Builder: All completed for {fuzz_target} {experiment} {fuzzer} {trial_num}: {len(all_mut_ids)} mutants in {time.time() - build_start_time:.2f}s.")
 
 
 if __name__ == "__main__":
