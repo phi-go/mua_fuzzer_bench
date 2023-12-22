db90dcc0e3495df88adb4881fb899640a283851c
phi-go <github@philipp-goerz.com>
2023-12-22 16:02:18 +0100

    use /mua_build/ instead of /tmp/test/

--- mua_build_ids.py
+++ mua_build_ids.py
@@ -113,7 +113,7 @@ def build_mutant(mut_id, exec_id, compile_db_path, mutants_dir, fuzz_target, deb
         '--result-path',
         mutant_file,
         '--statsdb',
-        '/tmp/test/stats.db',
+        '/mua_build/stats.db',
         '--mutation-list',
         config_file
     ]
