8b8b30c4835c1cdd33cfe2167b1b85429ed54092
phi-go <github@philipp-goerz.com>
2023-12-22 17:04:22 +0100

    add another layer of build

--- fuzzbench_build.sh
+++ fuzzbench_build.sh
@@ -6,6 +6,6 @@ set -Eeuox pipefail
 /bin/mua_build_benchmark
 cd /mutator && gradle build
 ldconfig /mutator/build/install/LLVM_Mutation_Tool/lib/
-pipx run hatch run src/mua_fuzzer_benchmark/eval.py locator_local --config-path /mua_build/config.json --result-path /mua_build/
-cp /mua_build/progs/**/*.locator $OUT/
+pipx run hatch run src/mua_fuzzer_benchmark/eval.py locator_local --config-path /mua_build/config.json --result-path /mua_build/build/
+cp /mua_build/build/progs/**/*.locator $OUT/
 cp /mua_build/config.json /out/config.json
