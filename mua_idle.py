13b77966064b60700e714930ca636dd79e992cd4
phi-go <github@philipp-goerz.com>
2024-01-03 14:09:19 +0100

    less spammy mua_idle

--- mua_idle.py
+++ mua_idle.py
@@ -7,8 +7,8 @@ run_before_file = "/firstrun"
 print("Entering mua_idle.py")
 
 if(os.path.isfile(run_before_file)):
+    print("IDLE IDLE IDLE")
     while(True):
-        print("IDLE IDLE IDLE")
         time.sleep(1000)
 fp = open(run_before_file, 'x')
 fp.close()
